b347a5d59de0d01733ed588768756239dc39579c
alghurabi01 <zaid.alghurabi01@gmail.com>
2024-10-10 22:39:25 -0400

    icnoss

--- internal/fileStorage/shared/loggers.go
+++
@@ -1,7 +0,0 @@
-package shared
-
-type Loggers struct{}
-
-
-	var InfoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-	errorLog := log.New(os.Stderr, "Error\t", log.Ldate|log.Ltime|log.Lshortfile)
